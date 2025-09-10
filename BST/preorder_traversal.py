bdda6f6e5519b1dc09332f29bac18d843ffb3451
ds <dav.silveira@proton.me>
2025-09-09 20:08:09 -0400

    solved the BST preorder traversal problem

---
+++ BST/preorder_traversal.py
@@ -0,0 +1,29 @@
+# Time: O(n) -> depends on size of nums array passed in
+# Space: O(h) -> since recursion creates a call stack O(h) is used where:
+#                   - balanced tree == O(log n) 
+#                   - skewed tree == O(n)
+#-
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def preorder_traversal(root):
+    if not root:
+        return []
+
+    res = []
+    def traverse(node):
+        if not node:
+            return
+
+        res.append(node.val)
+        traverse(node.left)
+        traverse(node.right)
+    traverse(root)
+    return res
+
+if __name__ == "__main__":
+    preorder_traversal()
